e53ea76fa6356d65d5c46761c1d22f5f0a8fc644
Dimitar <dim.bakardzhiev@outlook.com>
2018-06-07 17:16:03 +0100

    first commit

---
+++ SimpleCalculator.Test.Unit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.Test.Unit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("724356c9-9a09-4a7b-8df0-5a277d03e763")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SimpleCalculator/StringToIntConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class StringTointConverter
+    {
+        public int convert(string input)
+        {
+            try
+            {
+                int convertedNumber;
+                bool isConvertedSuccessfully = int.TryParse(input, out convertedNumber);
+
+                if (!isConvertedSuccessfully)
+                {
+                    throw new Exception("Conversion was not successful.");
+                }
+
+                return convertedNumber;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
